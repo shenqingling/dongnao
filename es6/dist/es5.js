663f4ae69a1b456fb050d6f95dfa7526b1a135e7
shenqingling <sql370@qq.com>
2017-03-13 23:05:12 +0800

    增加es6编译babel配置；

---
+++ es6/dist/es5.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var a = 'q';
+console.log(a);
+
+[1, 2, 3].map(function (v) {
+	console.log(v);
+});
+
+console.log('hello world');
+
+document.body.innerText = 'hello';
+
+{
+	var _fruit = 'apple';
+}
+
+console.log(fruit);
---
+++ es6/es6.js
@@ -0,0 +1,16 @@
+let a = 'q';
+console.log(a);
+
+[1,2,3].map((v)=>{
+	console.log(v);
+});
+
+console.log('hello world')
+
+document.body.innerText = 'hello'
+
+{
+	let fruit = 'apple';
+}
+
+console.log(fruit);
