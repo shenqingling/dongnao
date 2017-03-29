c3cdd76b0830ea2d6b5c47ec0082affbde4855fc
shenqingling <sql370@qq.com>
2017-03-29 22:27:05 +0800

    增加webpack打包功能

--- timer-react/src/index.js
+++ timer-react/src/index.js
@@ -1,9 +1,36 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import { AppContainer } from 'react-hot-loader';
+// AppContainer 是一个 HMR 必须的包裹(wrapper)组件
+
+import App from './components/App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+// 模块热替换的 API
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App)
+  });
+}
